1bd6579d51c239ea37600799b38399491e1b152a
smkrep <smokedrepublic1338@gmail.com>
2024-09-06 12:27:49 +0300

    finalized task1

--- task1/task.py
+++ task1/task.py
@@ -1,9 +1,33 @@
 import json
 
+def make_node(parent, children):
+    node = {}
+    node["parent"] = parent
+    node["children"] = children
+    return node
+
+
+def rec_parse_input(parent, data, graph):
+    desc = list(data.keys())
+    for key, value in data.items():
+        if value:
+            graph[f"{key}"] = make_node(parent, rec_parse_input(key, value, graph))
+        else:
+            graph[f"{key}"] = make_node(parent, [])
+    return desc
+
+
 def json_to_tree(json_string):
+    graph = {}
     data = json.loads(json_string)
+    rec_parse_input(None, data, graph)
+    return graph
+    
 
 
-test_string = "{ a: {}}"
+#test code
+test_string = '''{ "a": {"b": {"c": {}}}}'''
 
-json_to_tree(test_string)
+graph_structure = json_to_tree(test_string)
+
+print(graph_structure)
