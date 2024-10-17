fbcd78fd1fdcec0e22983abc56afe8d3c213b6c1
smkrep <smokedrepublic1338@gmail.com>
2024-10-17 11:14:13 +0300

    fixed relation matrix calculation

--- task2/task.py
+++ task2/task.py
@@ -28,11 +28,6 @@ def siblings_list(graph, curr_key):
     return [key for key in graph if graph[key]["parent"] == graph[curr_key]["parent"] and curr_key != key]
 
 
-def calculate_siblings(graph, curr_key):
-    if curr_key is None: return 0
-    return len(siblings_list(graph, curr_key))
-
-
 def calculate_parents(key: str, graph_structure):
     k = key
     counter = 0
@@ -40,16 +35,6 @@ def calculate_parents(key: str, graph_structure):
         counter+=1
         k = graph_structure[k]["parent"]
     return counter
-
-
-def calculate_siblings_children(graph_structure, siblings, c):
-    for sibling in siblings:
-        if len(graph_structure[sibling]["children"]) != 0:
-            for elem in graph_structure[sibling]["children"]:
-                calculate_siblings_children(graph_structure, graph_structure[sibling]["children"], c)
-                c.counter+=1
-        else:
-            return
     
 
 def calculate_indirect_children(graph_structure, children, c):
@@ -71,10 +56,9 @@ def calculate_relations(graph_structure):
     for curr_key, value in graph_structure.items():
         ans[0][int(curr_key) - 1] = 1 if value["parent"] is not None else 0 #Непосредственное управление
         ans[1][int(curr_key) - 1] = len(value["children"]) #Непосредственное подчинение
-        ans[2][int(curr_key) - 1] = calculate_parents(curr_key, graph_structure) + calculate_siblings(graph_structure, graph_structure[curr_key]["parent"]) - 1 if value["parent"] is not None else 0 # Опосредованное управление
+        ans[2][int(curr_key) - 1] = calculate_parents(curr_key, graph_structure) - 1 if value["parent"] is not None else 0 # Опосредованное управление
         siblings = siblings_list(graph_structure, curr_key)
         counter = Counter()
-        calculate_siblings_children(graph_structure, siblings, counter)
         for child in graph_structure[curr_key]["children"]:
             calculate_indirect_children(graph_structure, graph_structure[child]["children"], counter)
         ans[3][int(curr_key) - 1] = counter.counter #Опосредованное подчинение
